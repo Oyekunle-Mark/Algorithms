99defc74ba46f21be77581cfdae2f319f961f42d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-10-10 08:55:08 +0100

    Completed the rock paper scissors algorithm

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,34 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    # the keywords
+    keyWords = ['rock', 'paper', 'scissors']
+    # will hold the combinations
+    result = []
+    turns = n
+
+    def generateCombinations(turnsLeft, game):
+        # base case
+        if turnsLeft == 0:
+            result.append(game)
+            return
+
+        for i in keyWords:
+            # recursive case
+            generateCombinations(turnsLeft - 1, game + [i])
+
+    generateCombinations(turns, [])
+    return result
+
+
+print(rock_paper_scissors(2))
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
